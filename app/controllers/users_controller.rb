f5f0140f41f1a8a618ba70a6b883a06143c73008
Elizabeth Kosowski <elizabethkosowski@gmail.com>
2019-04-28 16:01:53 -0400

    my groups working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,11 @@ class UsersController < ApplicationController
     render json: @users
   end
 
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
   def create
     user = User.create(user_params)
 
