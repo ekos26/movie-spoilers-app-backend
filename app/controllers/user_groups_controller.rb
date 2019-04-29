e77f1a7abe66769cdcfb27d60205820b8c18a101
Elizabeth Kosowski <elizabethkosowski@gmail.com>
2019-04-28 22:53:29 -0400

    able to join group but getting end of json output error

--- app/controllers/user_groups_controller.rb
+++ app/controllers/user_groups_controller.rb
@@ -1,2 +1,18 @@
 class UserGroupsController < ApplicationController
+
+  def index
+    @user_groups = UserGroup.all
+    render json: @user_groups
+  end
+
+  def create
+    @user_group = UserGroup.create(user_groups_params)
+  end
+
+  private
+  
+  def user_groups_params
+    params.require(:user_group).permit(:user_id, :group_id)
+  end
+
 end
