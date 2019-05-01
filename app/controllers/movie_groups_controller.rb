38c7beca2b81875170e8b626d455dda294264e4a
Elizabeth Kosowski <elizabethkosowski@gmail.com>
2019-04-30 21:06:52 -0400

    think I figured out the problem with trying to add a movie to a group, lack of id

--- app/controllers/movie_groups_controller.rb
+++ app/controllers/movie_groups_controller.rb
@@ -5,4 +5,16 @@ class MovieGroupsController < ApplicationController
     render json: @movie_groups
   end
 
+  def create
+    byebug
+    @movie_group = MovieGroup.create(movie_groups_params)
+    render json: @movie_group
+  end
+
+  private
+
+  def movie_groups_params
+    params.require(:movie_group).permit(:movie_id, :group_id)
+  end
+
 end
